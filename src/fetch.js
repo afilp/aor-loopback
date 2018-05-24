50bf92d05f57aa01a0b23ca5fb63e698305fb8cc
afilp <afilp@users.noreply.github.com>
2018-05-25 00:16:12 +0300

    Update fetch.js
    Loopback returns `error`, which is an object with the `message` key inside. 
    
    I was about to code it directly as `json.error.message` but I was not sure if the server also sends `json.message` for some status codes. So I decided to keep both with an OR. Feel free to change this.

--- src/fetch.js
+++ src/fetch.js
@@ -35,7 +35,7 @@ export const fetchJson = (url, options = {}) => {
                 // not json, no big deal
             }
             if (status < 200 || status >= 300) {
-                return Promise.reject(new HttpError((json && json.message) || statusText, status));
+                return Promise.reject(new HttpError((json && (json.message || json.error.message)) || statusText, status));
             }
             return { status, headers, body, json };
         });
